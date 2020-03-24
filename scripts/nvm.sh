a09af0ff99d641e7e50e333b271d6e28940ede46
jameslkingsley <james.kingsley@routenote.com>
2020-03-24 13:48:55 +0000

    WIP

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-export NVM_DIR="/root/.nvm"
-
-echo https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh
-
 curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash
-source $NVM_DIR/nvm.sh
 
-echo "export NVM_DIR=${NVM_DIR}" >> $HOME/.bashrc
-echo 'source $NVM_DIR/nvm.sh' >> $HOME/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
